cabe7b9bc83187441f1c0e4c0ab9f5a414f52cab
Sylvain Gadrat <sgadrat@wontfix.it>
2017-04-23 19:16:30 +0200

    compatibility with apple's shitty products ;)

--- js/input.js
+++ js/input.js
@@ -7,23 +7,34 @@ var Input = {
 	},
 
 	keydown: function(event) {
-		 if (event.defaultPrevented) {
-		 	return;
-		 }
+		if (event.defaultPrevented) {
+			return;
+		}
 
-		 switch (event.key) {
-		 	case 'ArrowDown':
+		var key = null;
+		if (typeof undefined != 'undefined') {
+			key = event.key;
+		}else {
+			key = event.keyCode;
+		}
+
+		switch (key) {
+			case 'ArrowDown':
+			case 0x28:
 				Input.callbacks.move({x: 0, y: 1});
 				break;
-		 	case 'ArrowLeft':
+			case 'ArrowLeft':
+			case 0x25:
 				Input.callbacks.move({x: -1, y: 0});
 				break;
-		 	case 'ArrowUp':
+			case 'ArrowUp':
+			case 0x26:
 				Input.callbacks.move({x: 0, y: -1});
 				break;
-		 	case 'ArrowRight':
+			case 'ArrowRight':
+			case 0x27:
 				Input.callbacks.move({x: 1, y: 0});
 				break;
-		 }
+		}
 	},
 };
